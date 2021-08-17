9f3f9b0612add10758fff879633389d2caccea73
Matthew Tabert <37879493+flubadubdub@users.noreply.github.com>
2021-08-16 17:46:20 -0700

    initial commit

---
+++ apps/murder-hobo-maker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'matabert-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'murder-hobo-maker';
+}
